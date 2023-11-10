1cfece5ce31532cc22ca855d941431e1f44d97a1
Maoricio <31282531+Maoricio@users.noreply.github.com>
2023-11-09 23:08:12 -0300

    mejorar contador

--- src/components/countdown/clock/useCountdown.js
+++ src/components/countdown/clock/useCountdown.js
@@ -1,41 +1,33 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 
 export function useCountdown(targetDate) {
-  const [millisecondsTillTargetTime, setMillisecondsTillTargetTime] = useState(
-    targetDate.getTime() - new Date().getTime()
-  );
+  const calculateTimeLeft = useCallback(() => {
+    const difference = targetDate.getTime() - new Date().getTime();
+
+    if (difference > 0) {
+      const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+      return { days, hours, minutes, seconds };
+    }
+
+    // If the target date has passed, return all zeros or appropriate values
+    return { days: 0, hours: 0, minutes: 0, seconds: 0 };
+  }, [targetDate]);
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setMillisecondsTillTargetTime(
-        targetDate.getTime() - new Date().getTime()
-      );
+      setTimeLeft(calculateTimeLeft());
     }, 1000);
 
     return () => clearInterval(interval);
-  }, [targetDate]);
+  }, [calculateTimeLeft, targetDate]);
 
-  return getReturnValues(millisecondsTillTargetTime);
-}
-
-const getReturnValues = (millisecondsTillTargetTime) => {
-  // Calculate time left in various time intervals
-  const days = round(millisecondsTillTargetTime / (1000 * 60 * 60 * 24));
-  const hours = round(
-    (millisecondsTillTargetTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  const minutes = round(
-    (millisecondsTillTargetTime % (1000 * 60 * 60)) / (1000 * 60)
-  );
-  const seconds = round((millisecondsTillTargetTime % (1000 * 60)) / 1000);
-
-  return { days, hours, minutes, seconds };
-};
-
-// Round down if positive number, round up if negative number
-function round(value) {
-  if (value > 0) {
-    return Math.floor(value);
-  }
-  return Math.ceil(value);
+  return timeLeft;
 }
